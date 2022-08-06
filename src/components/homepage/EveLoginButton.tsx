c4194ffa26ef26dd2c2cad986a2294412eba3686
Dario Vidas <dario.vidas92@gmail.com>
2022-08-06 00:54:23 -0700

    Add eve scopes to login

--- src/components/homepage/EveLoginButton.tsx
+++ src/components/homepage/EveLoginButton.tsx
@@ -1,4 +1,5 @@
 import { CLIENT_ID, SECRET, CALLBACK_URI, SSO_STATE } from 'lib/eve_sso/EveSsoConfig';
+import eveScopes from 'lib/eve_sso/eveScopes';
 import EveSso from 'lib/eve_sso/evesso';
 import eveLoginImage from 'assets/eve-sso-login-white-small.png';
 
@@ -16,7 +17,7 @@ export default function EveLoginButton() {
 
   // TODO SSO_STATE should be generated
   return (
-    <a href={sso.getRedirectUrl(SSO_STATE)}>
+    <a href={sso.getRedirectUrl(SSO_STATE, eveScopes)}>
       <img src={eveLoginImage} alt="Eve login" />
     </a>
   );
