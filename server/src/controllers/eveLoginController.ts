678579e37de9cb3733b1477449c1618b66c1e135
Dario Vidas <dario.vidas92@gmail.com>
2022-08-08 23:28:04 -0700

    Fix the fucking scopes yet again, for the 10th time

--- server/src/controllers/eveLoginController.ts
+++ server/src/controllers/eveLoginController.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import ESI, { Provider } from 'eve-esi-client';
 import { CLIENT_ID, SECRET, CALLBACK_URI, SSO_STATE } from '../eve_sso/EveSsoConfig';
-import { eveScopes } from '../eve_sso/eveScopes';
+import { industryJobsScope } from '../eve_sso/eveScopes';
 
 const route = Router();
 
@@ -16,7 +16,10 @@ const controller = (app: Router, provider: Provider) => {
   })
 
   route.get('/login_url', (req: Request, res: Response) => {
-    res.send(esi.getRedirectUrl(SSO_STATE, eveScopes));
+    // We can only send a single scope here. Sending multiple
+    // scopes will cause a 403 on ESI
+    // TODO parametrize the scope
+    res.send(esi.getRedirectUrl(SSO_STATE, industryJobsScope));
   });
 
   route.get('/sso_callback', async (req: Request, res: Response) => {
