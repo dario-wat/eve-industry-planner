cd94550cc2ee8926148afc83c4a39f5f302c7353
Dario Vidas <dario.vidas92@gmail.com>
2022-08-11 11:28:19 -0700

    setup mysql and connect to it

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,56 @@ import 'reflect-metadata';
 
 import cors from 'cors';
 import express from 'express';
+import mysql from 'mysql';
+import { Sequelize, DataTypes } from 'sequelize';
 import initEveLoginController from './controllers/eveLoginController';
 import initEveFetchDataController from './controllers/eveFetchDataController';
+
+
+
+const sequelize = new Sequelize(
+  'eve_industry_planner_db',
+  'root',
+  'KMkk%^FLt7%WyhHg8HcfMkShdH$tYw5Sq',
+  {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+  }
+);
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
+
+
+
+const TypeId = sequelize.define("TypeID", {
+  group_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  // release_date: {
+  //   type: DataTypes.DATEONLY,
+  // },
+  // subject: {
+  //   type: DataTypes.INTEGER,
+  // }
+}, {
+  tableName: 'type_ids'
+});
+
+sequelize.sync().then(() => {
+  console.log('TypeID table created successfully!');
+}).catch((error) => {
+  console.error('Unable to create table : ', error);
+});
 
 const app = express();
 app.use(cors());
