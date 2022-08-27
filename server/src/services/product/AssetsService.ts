11a3552c232c00c47e6db50f20eaa032c30c86de
Dario Vidas <dario.vidas92@gmail.com>
2022-08-26 19:22:20 -0700

    somewhat fix assets

--- server/src/services/product/AssetsService.ts
+++ server/src/services/product/AssetsService.ts
@@ -28,11 +28,7 @@ export default class AssetsService {
     token: Token,
     assets: EveAsset[],
   ): Promise<EveAssetsRes> {
-    const assetMap = mapify(assets, 'item_id');
-    const assetsWithParent = assets.map(asset => ({
-      asset,
-      parent: assetMap[asset.location_id],
-    }));
+    // 1. Mapping data preparation
 
     // TODO(EIP-15) use sequelize join to combine the two below
     const typeIds = assets.map(a => a.type_id);
@@ -49,34 +45,53 @@ export default class AssetsService {
     );
     const typeCategoryMap = mapify(typeCategory, 'item_id');
 
+    // 2. Filtering down
+
+    const assetMap = mapify(assets, 'item_id');
+    const assetsWithParent = assets.map(asset => ({
+      asset,
+      parent: assetMap[asset.location_id] === undefined
+        ? null
+        : assetMap[asset.location_id],
+    }));
+
+    // 1. Asset without parent: 
+    //    either inside station, structure, pos(?), customs office(?)
+    // 2. Asset with parent:
+    //    inside a container, ship or container inside a ship,
+    //    the last case is when the parent has its own parent
+    // I want to include only items inside stations.
+
+
     // Filter out all assets whose parent doesn't exist. Those are root
-    // assets whose location must be station/structure or similar.
+    // assets whose location must be station, structure or similar.
     const rootLocationIds = uniq(assetsWithParent
-      .filter(o => o.parent === undefined)
+      .filter(o => o.parent === null)
       .map(o => o.asset.location_id),
     );
     const stationNames =
       await this.eveQuery.genAllStationNames(token, rootLocationIds);
 
-    // We don't care about items inside ships (fits, cargo, drones, ...)
-    const nonShipAssets = assetsWithParent.filter(o =>
-      o.parent
-        ? typeCategoryMap[o.parent.type_id].category_id !== SHIP_CAT
-        : true
-    );
-
-    // First check if the item is in the root location. If not then
-    // check the parent
-    const getLocationId = (o: any) =>
-      (rootLocationIds.includes(o.asset.location_id) && o.asset.location_id)
-      || (rootLocationIds.includes(o.parent.location_id) && o.parent.location_id)
-      || null;
-    return nonShipAssets.map(o => ({
-      name: types[o.asset.type_id] && types[o.asset.type_id].name,
-      type_id: o.asset.type_id,
-      quantity: o.asset.quantity,
-      location_id: getLocationId(o),
-      location: getLocationId(o) && stationNames[getLocationId(o)],
-    }));
+    return assetsWithParent
+      .filter(o => !(   // NOTE, below expression is negated
+        // No parent, but also not a station (pos, customs office)
+        (o.parent === null && stationNames[o.asset.location_id] === undefined)
+        // Has a parent, but parent is a ship (inside a ship)
+        || (o.parent && typeCategoryMap[o.parent.type_id].category_id === SHIP_CAT)
+        // Has a parent (container), but parent also has a parent (ship)
+        // (inside a container inside a ship)
+        || (o.parent && assetMap[o.parent.location_id] !== undefined)
+      ))
+      .map(o => ({
+        name: types[o.asset.type_id] && types[o.asset.type_id].name,
+        type_id: o.asset.type_id,
+        quantity: o.asset.quantity,
+        location_id:
+          (rootLocationIds.includes(o.asset.location_id) && o.asset.location_id)
+          || o.parent!.location_id,
+        location:
+          stationNames[o.asset.location_id]
+          || (o.parent && stationNames[o.parent.location_id])!,
+      }));
   }
 }
