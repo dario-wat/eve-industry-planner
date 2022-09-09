a8d4cc7c772601f01583301a996c3dd98e43b709
Dario Vidas <dario.vidas92@gmail.com>
2022-09-08 21:40:42 -0700

    Fix icon

--- client/src/components/homepage/HomePageAppBar.tsx
+++ client/src/components/homepage/HomePageAppBar.tsx
@@ -1,25 +1,16 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import ExtensionIcon from '@mui/icons-material/Extension';
 import EveLoginButton from 'components/homepage/EveLoginButton';
 
-// TODO(EIP-17) update icon, name and login
 export default function HomePageAppBar() {
   return (
     <AppBar
       position="fixed"
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
-        <IconButton
-          size="small"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
+        <ExtensionIcon sx={{ mr: 2 }} />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Eve Industry Planner
         </Typography>
