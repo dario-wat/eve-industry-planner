d4a09bde91e312998354ec0369cc1c7eb5a70a47
Dario Vidas <dario.vidas92@gmail.com>
2022-09-28 12:00:31 -0700

    Move planned product text area under the card file

--- client/src/components/dashboard/DashboardProductsCard.tsx
+++ client/src/components/dashboard/DashboardProductsCard.tsx
@@ -3,14 +3,21 @@ import {
   Card,
   CardContent,
   FormControlLabel,
+  Grid,
   Switch,
+  TextField,
   Typography,
 } from '@mui/material';
+import { withStyles } from '@material-ui/styles';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import useAxios from 'axios-hooks';
 import { useEffect, useState } from 'react';
-import { PlannedProductsRes } from '@internal/shared';
-import DashboardProductsTextArea from './DashboardProductsTextArea';
+import { filterNullOrUndef, PlannedProductsRes, PlannedProductsWithErrorRes } from '@internal/shared';
+import { useAppDispatch } from 'redux/hooks';
+import axios from 'axios';
+import { fetchProductionPlan } from 'redux/slices/productionPlanSlice';
+import { first } from 'underscore';
 
 export default function DashboardProductsCard() {
   const [{ data, loading }] = useAxios<PlannedProductsRes>('/planned_products');
@@ -99,5 +106,91 @@ function DashboardProductsDataGrid(
       disableColumnMenu
       experimentalFeatures={{ newEditingApi: true }}
     />
+  );
+}
+
+function DashboardProductsTextArea(
+  props: {
+    plannedProducts: PlannedProductsRes,
+    onUpdate: (plannedProducts: PlannedProductsRes) => void,
+  }
+) {
+  const [text, setText] = useState('');
+  useEffect(
+    () => setText(
+      props.plannedProducts.map(pp => pp.name + ' ' + pp.quantity).join('\n'),
+    ),
+    [props.plannedProducts],
+  );
+
+  const dispatch = useAppDispatch();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
+  const onButtonClick = async () => {
+    setIsSubmitting(true);
+    const { data } = await axios.post<PlannedProductsWithErrorRes>(
+      '/planned_products_recreate',
+      { text },
+    );
+
+    const errors = filterNullOrUndef(data.map(pp => pp.error));
+    if (errors.length === 0) {
+      // Update only when no errors
+      props.onUpdate(data.map(pp => ({
+        name: pp.name,
+        quantity: pp.quantity!,
+        stock: pp.stock!,
+      })));
+
+      // Trigger new production plan
+      dispatch(fetchProductionPlan());
+    }
+    setErrors(errors);
+    setIsSubmitting(false);
+  };
+
+  const RedTextTypography = withStyles({
+    root: {
+      color: 'red',
+    },
+  })(Typography);
+
+  return (
+    <Box>
+      <TextField
+        sx={{ pb: 2 }}
+        fullWidth
+        multiline
+        placeholder={
+          'Each line is a separate product.\n'
+          + 'Format: "product_name quantity" without quotes\n'
+          + 'E.g.\n'
+          + 'Nanofiber Internal Structure II 10\n'
+          + '1MN Afterburner II 5\n'
+          + 'Kikimora 1'
+        }
+        rows={15}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <LoadingButton
+            loading={isSubmitting}
+            variant="contained"
+            onClick={onButtonClick}
+          >
+            Submit
+          </LoadingButton>
+        </Grid>
+        <Grid item xs={9} sx={{ display: 'flex' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <RedTextTypography variant='body2'>
+              {first(errors)}
+            </RedTextTypography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
