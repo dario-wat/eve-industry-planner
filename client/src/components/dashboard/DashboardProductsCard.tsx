05e92bd99889a9fa91418f1094d70ede45c50ddc
Dario Vidas <dario.vidas92@gmail.com>
2022-09-28 11:48:48 -0700

    Move planned product data grid under the card file

--- client/src/components/dashboard/DashboardProductsCard.tsx
+++ client/src/components/dashboard/DashboardProductsCard.tsx
@@ -6,10 +6,10 @@ import {
   Switch,
   Typography,
 } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import useAxios from 'axios-hooks';
 import { useEffect, useState } from 'react';
 import { PlannedProductsRes } from '@internal/shared';
-import DashboardProductsDataGrid from './DashboardProductsDataGrid';
 import DashboardProductsTextArea from './DashboardProductsTextArea';
 
 export default function DashboardProductsCard() {
@@ -56,5 +56,44 @@ export default function DashboardProductsCard() {
         }
       </CardContent>
     </Card>
+  );
+}
+
+function DashboardProductsDataGrid(
+  props: {
+    plannedProducts: PlannedProductsRes,
+  }
+) {
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Product',
+      width: 300,
+      sortable: false,
+    },
+    {
+      field: 'quantity',
+      headerName: 'Quantity',
+      width: 100,
+      sortable: false,
+      renderCell: params =>
+        <div style={{
+          color: params.row.stock >= params.row.quantity ? 'green' : 'default',
+        }}>
+          {Math.min(params.row.stock, params.row.quantity)
+            + ' / '
+            + params.row.quantity}
+        </div>
+    },
+  ];
+
+  return (
+    <DataGrid
+      rows={props.plannedProducts}
+      columns={columns}
+      disableSelectionOnClick
+      disableColumnMenu
+      experimentalFeatures={{ newEditingApi: true }}
+    />
   );
 }
