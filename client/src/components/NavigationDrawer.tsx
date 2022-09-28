0bb4215119b592895a419bc49ea366e298bb3b99
Dario Vidas <dario.vidas92@gmail.com>
2022-09-28 11:43:45 -0700

    Cleanup drawer code

--- client/src/components/NavigationDrawer.tsx
+++ client/src/components/NavigationDrawer.tsx
@@ -13,7 +13,6 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import CachedIcon from '@mui/icons-material/Cached';
 import axios from 'axios';
 import { ReactNode, useContext } from 'react';
-import { uniqueId } from 'underscore';
 import useAxios from 'axios-hooks';
 import { EvePortraitRes } from '@internal/shared';
 import { UserContext } from 'contexts/UserContext';
@@ -28,23 +27,7 @@ type Props = {
 
 export default function NavigationDrawer(props: Props) {
   const { tabs, selectedTab, onTabClick } = props;
-  const [{ data: portrait }] = useAxios<EvePortraitRes>('/portrait');
 
-  const userContext = useContext(UserContext);
-
-  const onLogoutClick = async () => {
-    const { status } = await axios.delete('/logout');
-    if (status === 200) {
-      window.location.reload();
-    }
-  };
-
-  const onClearCacheClick = async () => {
-    const { status } = await axios.delete('/clear_cache');
-    if (status === 200) {
-      window.location.reload();
-    }
-  };
   return (
     <Drawer
       variant="permanent"
@@ -79,42 +62,74 @@ export default function NavigationDrawer(props: Props) {
               </ListItem>
             ))}
             <Divider />
-            <ListItem key="clearCache" disablePadding>
-              <ListItemButton onClick={onClearCacheClick}>
-                <ListItemIcon>
-                  <CachedIcon />
-                </ListItemIcon>
-                <ListItemText primary="Clear Cache" />
-              </ListItemButton>
-            </ListItem>
+            <ClearCacheButtonListItem />
           </List>
         </Box>
         <Box>
           <List>
-            <ListItem key="loggedInUser" disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <ListItemAvatar>
-                    <Avatar
-                      alt=""
-                      src={portrait?.px64x64}
-                      sx={{ width: 36, height: 36 }} />
-                  </ListItemAvatar>
-                </ListItemIcon>
-                <ListItemText primary={userContext.character_name} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem key="logout" disablePadding>
-              <ListItemButton onClick={onLogoutClick}>
-                <ListItemIcon>
-                  <LogoutIcon />
-                </ListItemIcon>
-                <ListItemText primary="Logout" />
-              </ListItemButton>
-            </ListItem>
+            <LoggedInUserButtonListItem />
+            <LogoutButtonListItem />
           </List>
         </Box>
       </Box>
     </Drawer>
+  );
+}
+
+function LoggedInUserButtonListItem() {
+  const [{ data: portrait }] = useAxios<EvePortraitRes>('/portrait');
+  const userContext = useContext(UserContext);
+  return (
+    <ListItem key="loggedInUser" disablePadding>
+      <ListItemButton>
+        <ListItemIcon>
+          <ListItemAvatar>
+            <Avatar
+              alt=""
+              src={portrait?.px64x64}
+              sx={{ width: 36, height: 36 }} />
+          </ListItemAvatar>
+        </ListItemIcon>
+        <ListItemText primary={userContext.character_name} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function ClearCacheButtonListItem() {
+  const onClearCacheClick = async () => {
+    const { status } = await axios.delete('/clear_cache');
+    if (status === 200) {
+      window.location.reload();
+    }
+  };
+  return (
+    <ListItem key="clearCache" disablePadding>
+      <ListItemButton onClick={onClearCacheClick}>
+        <ListItemIcon>
+          <CachedIcon />
+        </ListItemIcon>
+        <ListItemText primary="Clear Cache" />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function LogoutButtonListItem() {
+  const onLogoutClick = async () => {
+    const { status } = await axios.delete('/logout');
+    if (status === 200) {
+      window.location.reload();
+    }
+  };
+  return (
+    <ListItem key="logout" disablePadding>
+      <ListItemButton onClick={onLogoutClick}>
+        <ListItemIcon>
+          <LogoutIcon />
+        </ListItemIcon>
+        <ListItemText primary="Logout" />
+      </ListItemButton>
+    </ListItem>
   );
 }
