9bbd9fb80f71fdfcc955f99a3600cbb57cbe3c78
Dario Vidas <dario.vidas92@gmail.com>
2022-09-29 21:57:54 -0700

    Add circular progress while logging in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,35 @@ import HomePage from 'components/homepage/HomePage';
 import { defaultUserContextValue, UserContext } from 'contexts/UserContext';
 import store from 'redux/store';
 import { EveLoggedInUserRes } from '@internal/shared';
+import { Box, CircularProgress, Toolbar } from '@mui/material';
 
 function App() {
-  const [{ data }] = useAxios<EveLoggedInUserRes>('/logged_in_user');
+  const [{ data, loading }] = useAxios<EveLoggedInUserRes>('/logged_in_user');
   const userContext = data
     ? {
       ...data,
       is_logged_in: data.character_id !== null,
     }
     : defaultUserContextValue;
+
   return (
     <Provider store={store}>
       <UserContext.Provider value={userContext}>
-        <HomePage />
+        {loading
+          ?
+          <Box sx={{ height: 300, width: 1 }}>
+            <Toolbar /> {/* need this to push the nav bar below the app bar */}
+            <Box
+              sx={{ height: '100%', width: 1 }}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <CircularProgress />
+            </Box>
+          </Box>
+          :
+          <HomePage />}
       </UserContext.Provider>
     </Provider>
   );
