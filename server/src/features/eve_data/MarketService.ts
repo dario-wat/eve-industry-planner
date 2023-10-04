64531e64433f9480ee4b8652655450cbcf344153
Dario Vidas <dario.vidas92@gmail.com>
2023-10-03 21:58:08 -0700

    Fix some helper function

--- server/src/features/eve_data/MarketService.ts
+++ server/src/features/eve_data/MarketService.ts
@@ -4,6 +4,7 @@ import EsiTokenlessQueryService from '../../core/query/EsiTokenlessQueryService'
 import EveQueryService from '../../core/query/EveQueryService';
 import { MarketOrdersRes } from '@internal/shared';
 import ActorContext from '../../core/actor_context/ActorContext';
+import { genQueryFlatPerCharacter } from '../../lib/eveUtil';
 
 
 @Service()
@@ -19,18 +20,9 @@ export default class MarketService {
   public async genMarketOrdersForPage(
     actorContext: ActorContext,
   ): Promise<MarketOrdersRes> {
-    const characters = await actorContext.genLinkedCharacters();
-
-    const characterOrders = await Promise.all(characters.map(
-      async character => ([
-        character,
-        await this.esiQuery.genxMarketOrders(character.characterId)
-      ] as const),
-    ));
-
-    // TODO move to util? there is another one laying around here
-    const orders = characterOrders.flatMap(([character, orders]) =>
-      orders.map(order => ([character, order] as const))
+    const orders = await genQueryFlatPerCharacter(
+      actorContext,
+      async character => await this.esiQuery.genxMarketOrders(character.characterId),
     );
 
     const stationNames = await this.eveQuery.genAllStationNames(
