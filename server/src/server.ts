69dd125c44894880383beeb418c7c9a5dcc0e305
Dario Vidas <dario.vidas92@gmail.com>
2023-11-16 22:08:40 -0800

    another cookie attempt

--- server/src/server.ts
+++ server/src/server.ts
@@ -47,7 +47,7 @@ async function init() {
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: true,
+      secure: false,
       maxAge: hoursToMilliseconds(7 * 24),
       // sameSite: 'none',
     },
