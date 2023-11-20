5f8b31fbe768450ac77f6ca978477c201d358667
Dario Vidas <dario.vidas92@gmail.com>
2023-11-19 21:20:34 -0800

    Direct fe requests to the correct backend endpoint

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://qmm534tebb.us-west-2.awsapprunner.com';
+// axios.defaults.baseURL = 'https://localhost:8080';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;    // needed for cookies
 
