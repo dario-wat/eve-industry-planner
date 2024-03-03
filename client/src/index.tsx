c843a2187403b06ce7d463025813e57751040220
Dario Vidas <dario.vidas92@gmail.com>
2024-03-02 16:59:22 -0800

    Add local change

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // TODO this needs to be swapped every time code is pushed :/
-axios.defaults.baseURL = 'https://qmm534tebb.us-west-2.awsapprunner.com';
-// axios.defaults.baseURL = 'http://localhost:8080';
+// axios.defaults.baseURL = 'https://qmm534tebb.us-west-2.awsapprunner.com';
+axios.defaults.baseURL = 'http://localhost:8080';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;    // needed for cookies
